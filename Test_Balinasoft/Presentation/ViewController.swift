648bd19a23cfd0f365a21c1019253d02b2d3fa09
Hleb <hlebgyrt@gmail.com>
2023-12-30 03:19:53 +0300

    First commit. Первоначальная настройка приложения

---
+++ Test_Balinasoft/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_Balinasoft
+//
+//  Created by Hleb Karpovich on 30.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
